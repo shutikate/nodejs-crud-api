68df7ff6e274cdcbdc6d7032309a30b02f5a05a3
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-18 01:30:34 +0200

    feat: create server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-const my = () => {
-  console.log('hello');
-};
-my();
+import 'dotenv/config';
+import http from 'node:http';
+
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
