dd801114c1ccc371773b585029898ac9a2fd5f16
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-20 15:29:42 +0200

    refactor: add line breaks in post file

--- src/endpoints/post.ts
+++ src/endpoints/post.ts
@@ -6,26 +6,36 @@ import { User } from '../data/types.ts';
 export const post = (url: string, req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
   if (url === '/api/users') {
     let body = '';
+
     req.on('data', (chunk) => {
       body += chunk;
     });
+
     req.on('end', () => {
+
       if (body) {
         const user: User = JSON.parse(body);
+
         if (isValidateUser(user)) {
           const id = v4();
           res.writeHead(201, { 'Content-Type': 'application/json' });
           addData(id, user);
           res.end(JSON.stringify({ id, ...user }));
-        } else {
+        }
+        else {
           res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ message: 'Invalid request: request does not contain required fields'}));
+          res.end(JSON.stringify({ message: 'Request does not contain required fields'}));
         }
       }
+
       else {
         res.writeHead(400, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Invalid request: request does not contain required fields'}));
+        res.end(JSON.stringify({ message: 'Request with empty body'}));
       }
     });
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Request with invalid url'}));
   }
 }
